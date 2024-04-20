f2dd9f2c4c16f5a91d12e279876d370c7700fb5b
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-04-21 02:22:00 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.03 ms, Memory: 75.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(String my_string, String is_suffix) {
+       int answer = 0;
+
+        String[] arr = new String[my_string.length()];
+
+        for (int i = 0; i < my_string.length(); i++) {
+            arr[i] = my_string.substring(i);
+        }
+        for (String suffixes : arr) {
+            if (suffixes.equals(is_suffix)) {
+                answer = 1;
+                break;
+            } else {
+                answer = 0;
+            }
+        }
+        return answer;
+    }
+}
