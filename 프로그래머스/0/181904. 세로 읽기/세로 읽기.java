92d9e3c050847147ee84e821aec38b117087a1db
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-04-21 04:40:43 +0900

    [level 0] Title: 세로 읽기, Time: 1.66 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181904. 세로 읽기/세로 읽기.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(String my_string, int m, int c) {
+         char[] arr = my_string.toCharArray();
+
+        String answer = "";
+        for (int i = c - 1; i < arr.length; i += m) {
+            answer += String.valueOf(arr[i]);
+        }
+        return answer;
+    }
+}
