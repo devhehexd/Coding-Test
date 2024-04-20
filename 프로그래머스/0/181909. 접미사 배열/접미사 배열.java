de1ffd1bcb2e6ba543f16f46f3f5040d67447574
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-04-21 01:30:42 +0900

    [level 0] Title: 접미사 배열, Time: 0.16 ms, Memory: 75.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String my_string) {
+       String[] answer = new String[my_string.length()];
+
+        for (int i = 0; i < my_string.length(); i++) {
+            String str = my_string.substring(i);
+            answer[i] = str;
+        }
+        Arrays.sort(answer);
+        return answer;
+    }
+}
