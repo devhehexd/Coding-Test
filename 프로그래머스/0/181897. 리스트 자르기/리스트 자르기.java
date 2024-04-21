5a4fc52f470039da89224e73d0c952881fd90439
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-04-21 18:07:33 +0900

    [level 0] Title: 리스트 자르기, Time: 2.59 ms, Memory: 71.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181897. 리스트 자르기/리스트 자르기.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public int[] solution(int n, int[] slicer, int[] num_list) {
+        List<Integer> list = new ArrayList<>();
+        int a = slicer[0], b = slicer[1], c = slicer[2];
+
+        if (n == 1) {
+            for (int i = 0; i <= b; i++) {
+                list.add(num_list[i]);
+            }
+        } else if (n == 2) {
+            for (int i = a; i < num_list.length; i++) {
+                list.add(num_list[i]);
+            }
+        } else if (n == 3) {
+            for (int i = a; i <= b; i++) {
+                list.add(num_list[i]);
+            }
+        } else if (n == 4) {
+            for (int i = a; i <= b; i += c) {
+                list.add(num_list[i]);
+            }
+        }
+        return list.stream().mapToInt(i -> i).toArray();
+    }
+}
