3e11d8e22f75ec0a51cfbe7f5e5c1994d8ae1ae0
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-07-19 13:56:45 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 11.46 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -1,25 +1,27 @@
 class Solution {
     public int solution(String myString, String pat) {
         int answer = 0;
-        if(myString.toLowerCase().contains(pat.toLowerCase())) {
-            answer = 1;
+
+        if (pat.length() > myString.length()) {
+            answer = 0;
+        } else {
+            char[] myStringChar = myString.toLowerCase().toCharArray();
+            char[] patChar = pat.toLowerCase().toCharArray();
+            int length = patChar.length;
+            for (int i = 0; i + length <= myStringChar.length; i++) {
+                boolean flag = false;
+                for (int j = i; j < i + length; j++) {
+                    if (myStringChar[j] != patChar[j - i]) {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag) {
+                    answer = 1;
+                    break;
+                }
+            }
         }
-
-//         if (pat.length() > myString.length()) {
-//             answer = 0;
-//         }
-
-//         for (int i = 0; i < myString.length(); i++) {
-//             int lastNum = pat.length() + i;
-//             String tmp = "";
-//             if (i <= myString.length() - pat.length()) {
-//                 tmp = myString.substring(i, lastNum);
-//             }
-//             if (lastNum < myString.length() && tmp.equalsIgnoreCase(pat)) {
-//                 answer = 1;
-//                 break;
-//             }
-//         }
         return answer;
     }
 }
