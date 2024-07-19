c7217fb009f022fd4ee9afe31d762d93db88fc8c
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-07-19 13:43:24 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 9.91 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        int answer = 0;
+        if(myString.toLowerCase().contains(pat.toLowerCase())) {
+            answer = 1;
+        }
+
+//         if (pat.length() > myString.length()) {
+//             answer = 0;
+//         }
+
+//         for (int i = 0; i < myString.length(); i++) {
+//             int lastNum = pat.length() + i;
+//             String tmp = "";
+//             if (i <= myString.length() - pat.length()) {
+//                 tmp = myString.substring(i, lastNum);
+//             }
+//             if (lastNum < myString.length() && tmp.equalsIgnoreCase(pat)) {
+//                 answer = 1;
+//                 break;
+//             }
+//         }
+        return answer;
+    }
+}
