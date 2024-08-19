c0728d8e22be619188450d4b3e065df554a58f2c
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-08-19 22:27:28 +0900

    [level 2] Title: 전화번호 목록, Time: 276.22 ms, Memory: 210 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String prefix : phone_book) {
+            map.put(prefix, 1);
+        }
+
+        for (String prefix : phone_book) {
+            for (int i = 1; i < prefix.length(); i++) {
+                if (map.containsKey(prefix.substring(0, i))) {
+                    answer = false;
+                    break;
+                } else {
+                    answer = true;
+                }
+            }
+            if (!answer) {
+                break;
+            }
+        }
+        return answer;
+    }
+}
