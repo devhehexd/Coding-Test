723c3fcbb27702191cffab59556647f94ea9b514
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-10-15 18:08:07 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.93 ms, Memory: 80.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int price) {
+        return price >= 500000 ? (int) Math.floor(price * 0.8) :
+                price >= 300000 ? (int) Math.floor(price * 0.9) :
+                        price >= 100000 ? (int) Math.floor(price * 0.95) : price;
+    }
+}
