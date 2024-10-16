4eeea8f82300222e0d3d318f86e9a5b498091db4
devhehexd <165916047+devhehexd@users.noreply.github.com>
2024-10-16 20:32:37 +0900

    [Silver IV] Title: 수 찾기, Time: 144 ms, Memory: 50380 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,29 +1,11 @@
-import sys
-input = sys.stdin.readline
+N = int(input())
+A = set(map(int, input().split()))
 
-N = int(input().strip())
+M = int(input())
+B = list(map(int, input().split()))
 
-# 정수가 저장된 리스트
-number_list = list(map(int, input().strip().split()))
-
-M = int(input().strip())
-
-# 타겟 정수가 저장된 리스트
-target_list = list(map(int, input().strip().split()))
-
-import bisect
-
-answer = 0
-
-sorted_number_list = sorted(number_list)
-# target들이 number_list에 존재하는가?
-for target in target_list:
-  index_left = bisect.bisect_left(sorted_number_list, target)
-  # index_right = bisect.bisect_right(sorted_number_list, target)
-  
-  if index_left != len(sorted_number_list) and sorted_number_list[index_left] == target:
-    answer = 1
-    print(answer)
+for X in B:
+  if X in A:
+    print(1)
   else:
-    answer = 0
-    print(answer)
+    print(0)
